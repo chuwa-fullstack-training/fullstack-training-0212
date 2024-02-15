ddc2873fa26afeeb70ab020175af6d84d4e7ca2c
machearn <machearn.n@gmail.com>
2024-02-15 00:23:02 -0500

    feat: finished hw4 of lec2

--- homeworks/lecture2/hw4.js
+++ homeworks/lecture2/hw4.js
@@ -8,6 +8,8 @@ function f() {
   }
   console.log(a);
 }
+// 7
+// explanation: the variable a is changed to 7 by the if statement.
 
 // 2. When executed, what value will be output?
 function f() {
@@ -16,6 +18,8 @@ function f() {
   }
   console.log(a);
 }
+// 5
+// explanation: the variable declared with var is function-scoped, not block-scoped.
 
 // 3. When executed, what value will be output?
 function f() {
@@ -23,6 +27,8 @@ function f() {
 }
 f();
 console.log(a);
+// 3
+// explanation: The expression a = 3 creates a global variable a.
 
 // 4.
 var a = 5;
@@ -35,6 +41,8 @@ function second() {
 }
 first();
 second();
+// 6
+// explanation: The global variable a is changed to 6 by the function first().
 
 // 5.
 var a = 5;
@@ -42,6 +50,8 @@ function f() {
   var a = 7;
   console.log(a);
 }
+// 7
+// explanation: The local variable a in the function f() has the value 7.
 
 // 6.
 var a = 1;
@@ -52,3 +62,16 @@ function b() {
 }
 b();
 console.log(a);
+// 1
+/*
+ * explanation:
+ * The function a() is hoisted to the top of the function b().
+ * So, the function b() becomes:
+ * function b() {
+ *  function a() {}
+ *  a = 10;
+ *  return;
+ *  }
+ *  The expression a = 10 changes the value of the local variable a in the function b() to 10.
+ *  instead of the global variable a.
+ */
