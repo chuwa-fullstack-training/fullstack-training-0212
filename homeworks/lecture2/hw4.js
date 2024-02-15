f2483aaec5cff7f18917d8a922684c6dc8b8d7b8
xuze19 <159867185+xuze19@users.noreply.github.com>
2024-02-15 16:17:01 -0500

    Update hw4.js

--- homeworks/lecture2/hw4.js
+++ homeworks/lecture2/hw4.js
@@ -8,6 +8,11 @@ function f() {
   }
   console.log(a);
 }
+/*
+7
+cuz var a defined a as a function scope variable which is hoisted to the top of the function,
+by if statement, it's greater than 5, so re-assigned to 7 
+*/
 
 // 2. When executed, what value will be output?
 function f() {
@@ -16,6 +21,11 @@ function f() {
   }
   console.log(a);
 }
+/*
+5
+cuz var a defined a as a function scope variable which is hoisted to the top of the function,
+by if statement, it's always true, so a assigned to 5
+*/
 
 // 3. When executed, what value will be output?
 function f() {
@@ -23,6 +33,10 @@ function f() {
 }
 f();
 console.log(a);
+/*
+3
+cuz a didn't given a keyword, making it as a global variable.
+*/
 
 // 4.
 var a = 5;
@@ -35,6 +49,10 @@ function second() {
 }
 first();
 second();
+/*
+6
+cuz a in function first () didn't given a keyword, making a as global variable covers original golbal a = 5
+*/
 
 // 5.
 var a = 5;
@@ -42,6 +60,10 @@ function f() {
   var a = 7;
   console.log(a);
 }
+/*
+7
+cuz console.log in the function, prints the function variable a = 7 
+*/
 
 // 6.
 var a = 1;
@@ -52,3 +74,13 @@ function b() {
 }
 b();
 console.log(a);
+/*
+1
+function a is hoisted to the top of function b as
+function b() {
+  function a() {}
+  a = 10; 
+  return;
+}
+a = 10 assigns 10 to the function name a
+*/
