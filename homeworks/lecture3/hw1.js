e035d26489c3e4389b18cfafd43738009d85b5ad
machearn <machearn.n@gmail.com>
2024-02-16 21:14:30 -0500

    feat: finished hw1 of lec3

--- homeworks/lecture3/hw1.js
+++ homeworks/lecture3/hw1.js
@@ -1,8 +1,23 @@
-/** 
+/**
  * there are unlimited numbers of 1c, 5c, 25c, 50c
  * pick 48 coins to have 1 dollar
  * print out 2 solutions
  */
 function pickCoins() {
-    // implement here
+  // implement here
+  let result = [];
+  for (let i = 0; i <= 48; i++) {
+    for (let j = 0; j <= 24; j++) {
+      for (let k = 0; k <= 9; k++) {
+        for (let l = 0; l <= 1; l++) {
+          if (i + 5 * j + 25 * k + 50 * l === 100 && i + j + k + l === 48) {
+            result.push([i, j, k, l]);
+            if (result.length === 2) {
+              return result;
+            }
+          }
+        }
+      }
+    }
+  }
 }
