ff179f1ae8260b833924ec49296b13e1466ac15b
machearn <machearn.n@gmail.com>
2024-02-17 01:20:05 -0500

    feat: finished hw4 of lec3

--- homeworks/lecture3/hw4.js
+++ homeworks/lecture3/hw4.js
@@ -1,16 +1,16 @@
 function Shape() {
-    this.type = 'shape';
+  this.type = "shape";
 }
 
-Shape.prototype.getType = function() {
-    return this.type;
-}
+Shape.prototype.getType = function () {
+  return this.type;
+};
 
 function Triangle(a, b, c) {
-    this.type = 'triangle';
-    this.a = a;
-    this.b = b;
-    this.c = c;
+  this.type = "triangle";
+  this.a = a;
+  this.b = b;
+  this.c = c;
 }
 
 Triangle.prototype = Object.create(Shape.prototype);
@@ -25,3 +25,69 @@ Triangle.prototype.constructor = Triangle;
 // 5. implement a method circumference for Circle class
 
 // 6. change all code above to use ES6 class syntax
+
+Triangle.prototype.getPerimeter = function () {
+  return this.a + this.b + this.c;
+};
+
+Triangle.prototype.getArea = function () {
+  const s = this.getPerimeter() / 2;
+  return Math.sqrt(s * (s - this.a) * (s - this.b) * (s - this.c));
+};
+
+function Circle(radius) {
+  this.type = "circle";
+  this.radius = radius;
+}
+
+Circle.prototype = Object.create(Shape.prototype);
+Circle.prototype.constructor = Circle;
+
+Circle.prototype.area = function () {
+  return Math.PI * this.radius ** 2;
+};
+
+Circle.prototype.circumference = function () {
+  return 2 * Math.PI * this.radius;
+};
+
+class Shape {
+  constructor() {
+    this.type = "shape";
+  }
+  getType() {
+    return this.type;
+  }
+}
+
+class Triangle extends Shape {
+  constructor(a, b, c) {
+    super();
+    this.type = "triangle";
+    this.a = a;
+    this.b = b;
+    this.c = c;
+  }
+  getPerimeter() {
+    return this.a + this.b + this.c;
+  }
+  getArea() {
+    const s = this.getPerimeter() / 2;
+    return Math.sqrt(s * (s - this.a) * (s - this.b) * (s - this.c));
+  }
+}
+
+class Circle extends Shape {
+  constructor(radius) {
+    super();
+    this.type = "circle";
+    this.radius = radius;
+  }
+  area() {
+    return Math.PI * this.radius ** 2;
+  }
+  circumference() {
+    return 2 * Math.PI * this.radius;
+  }
+}
+
