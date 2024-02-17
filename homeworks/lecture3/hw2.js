b0a00d85de84a3f3f60a45df9c607e74f5079c8f
machearn <machearn.n@gmail.com>
2024-02-17 01:19:36 -0500

    feat: finished hw2 of lec3

--- homeworks/lecture3/hw2.js
+++ homeworks/lecture3/hw2.js
@@ -3,5 +3,12 @@
  * console.log(sum(2, 3) === 5)
  */
 function sum() {
-    // implement here
+  // implement here
+  if (arguments.length === 1) {
+    return function (b) {
+      return arguments[0] + b;
+    };
+  } else {
+    return arguments[0] + arguments[1];
+  }
 }
