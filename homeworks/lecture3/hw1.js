f74d21bd435835bad7ed4609145f1d730f5269dd
xuze19 <159867185+xuze19@users.noreply.github.com>
2024-02-18 15:19:55 -0500

    Update hw1.js

--- homeworks/lecture3/hw1.js
+++ homeworks/lecture3/hw1.js
@@ -5,4 +5,22 @@
  */
 function pickCoins() {
     // implement here
+    let res = [];
+    for (let c1 = 0; c1 <= 48; c1++) {
+        for (let c5 = 0; c5 <= 48 - c1; c5++) {
+            for (let c25 = 0; c25 <= 48 - c1 - c5; c25 ++) {
+                total = c1 + 5 * c5 + 25 * c25 + 50 * (48 - c1 - c5 - c25)
+                if (total === 100) {
+                    res.push([c1, c5, c25, 48 - c1 - c5 - c25]);
+                }
+                if (res.length === 2) {
+                    console.log(res);
+                    return;
+                }
+            }
+        }
+    }
+    return res;
 }
+
+pickCoins();
