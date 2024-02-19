dbd00e972be81e596845f330d1166c59c1a374d1
CSHangyuanXu <47225065+CSHangyuanXu@users.noreply.github.com>
2024-02-19 14:27:27 -0800

    Update hw4.js

--- homeworks/lecture2/hw4.js
+++ homeworks/lecture2/hw4.js
@@ -2,53 +2,63 @@
 
 // 1. When executed, what value will be output?
 function f() {
-  var a = 10;
-  if (a > 5) {
-    a = 7;
+    var a = 10;
+    if (a > 5) {
+      a = 7;
+    }
+    console.log(a);
   }
-  console.log(a);
-}
+  // ans:7
 
-// 2. When executed, what value will be output?
-function f() {
-  if (true) {
-    var a = 5;
+
+  // 2. When executed, what value will be output?
+  function f() {
+    if (true) {
+      var a = 5;
+    }
+    console.log(a);
   }
+
+  // ans: 5
+  
+  // 3. When executed, what value will be output?
+  function f() {
+    a = 3;
+  }
+  f();
   console.log(a);
-}
+  //ans : 3
+  
+  // 4.
+  var a = 5;
+  function first() {
+    a = 6;
+  }
 
-// 3. When executed, what value will be output?
-function f() {
-  a = 3;
-}
-f();
-console.log(a);
+  
+  function second() {
+    console.log(a);
+  }
+  first();
+  second();
+ //ans :   6    
 
-// 4.
-var a = 5;
-function first() {
-  a = 6;
-}
 
-function second() {
+  // 5.
+  var a = 5;
+  function f() {
+    var a = 7;
+    console.log(a);
+  }
+  // ans : 7
+  
+  // 6.
+  var a = 1;
+  function b() {
+    a = 10;
+    return;
+    function a() {}
+  }
+  b();
   console.log(a);
-}
-first();
-second();
-
-// 5.
-var a = 5;
-function f() {
-  var a = 7;
-  console.log(a);
-}
-
-// 6.
-var a = 1;
-function b() {
-  a = 10;
-  return;
-  function a() {}
-}
-b();
-console.log(a);
+  //ans :  10
