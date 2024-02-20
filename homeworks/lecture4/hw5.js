609f911d74101b36fdc99fafd6a156afbba472d6
machearn <machearn.n@gmail.com>
2024-02-20 18:21:26 -0500

    feat: finished hw5 for lec4

--- homeworks/lecture4/hw5.js
+++ homeworks/lecture4/hw5.js
@@ -1,11 +1,29 @@
 // write a function to deep clone an object with circular reference
-// 
+//
 // const data = {
 //     name: 'foo',
 //     child: null
 // }
 // data.child = data;
 
+let cloned = new Map();
 const cloneDeepWithLoop = (obj) => {
-    // Implement the function here
-}
+  // Implement the function here
+  if (cloned.has(obj)) {
+    return cloned.get(obj);
+  }
+  let newObj = {};
+  cloned.set(obj, newObj);
+  for (let key in obj) {
+    if (obj[key] === null) {
+      newObj[key] = null;
+    } else if (typeof obj[key] === "object") {
+      newObj[key] = cloneDeepWithLoop(obj[key]);
+    } else {
+      newObj[key] = obj[key];
+    }
+  }
+
+  return newObj;
+};
+
