0a57af6e173fe2e218e65fd4b2e8586952ff146a
machearn <machearn.n@gmail.com>
2024-02-20 18:20:37 -0500

    feat: finished hw1 for lec4

--- homeworks/lecture4/hw1.js
+++ homeworks/lecture4/hw1.js
@@ -6,5 +6,23 @@
 // <html><head><title>My Title</title></head></html - true
 
 function checkValidHTML(html) {
-    // implement your solution here
+  // implement your solution here
+  const stack = [];
+  const openingTag = /<([^/>]+)>/g;
+  const closingTag = /<\/([^/>]+)>?/g;
+  let match;
+
+  while ((match = openingTag.exec(html)) !== null) {
+    stack.push(match[1]);
+  }
+  while ((match = closingTag.exec(html)) !== null) {
+    if (stack.length === 0) return false;
+    if (stack[stack.length - 1] === match[1]) {
+      stack.pop();
+    } else {
+      return false;
+    }
+  }
+
+  return stack.length === 0;
 }
