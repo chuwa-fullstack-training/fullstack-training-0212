b80469d13479c64b854d02982090cd895c285d74
machearn <machearn.n@gmail.com>
2024-02-20 18:21:16 -0500

    feat: finished hw4 for lec4

--- homeworks/lecture4/hw4.js
+++ homeworks/lecture4/hw4.js
@@ -12,4 +12,15 @@
  */
 const intersection = (nums1, nums2) => {
   // Your solution here
+  let result = [];
+  let set1 = new Set(nums1);
+  let set2 = new Set(nums2);
+
+  for (let num of set1) {
+    if (set2.has(num)) {
+      result.push(num);
+    }
+  }
+
+  return result;
 };
