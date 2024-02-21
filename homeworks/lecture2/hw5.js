58bc6b253ef75891ca35a2434654d6f09c10fd08
CSHangyuanXu <47225065+CSHangyuanXu@users.noreply.github.com>
2024-02-21 15:29:41 -0800

    Update hw5.js

--- homeworks/lecture2/hw5.js
+++ homeworks/lecture2/hw5.js
@@ -1,26 +1,28 @@
-// Hoisting
-
-// 1.
 var x;
 
 if (x !== 3) {
-  console.log(y);
+  console.log(y); //undefined 
   var y = 5;
   if (y === 5) {
     var x = 3;
   }
-  console.log(y);
+  console.log(y); // 5
 }
 if (x === 3) {
-  console.log(y);
+  console.log(y); // 5
 }
 
+// "The variable y is initially undefined because, due to hoisting,
+//  y is declared before the code is executed but not yet assigned any value.
+//   Once the execution reaches y = 5, y is assigned the value 5, and from that point onwards, the value of y will be 5.
 
-// 2.
 var x = 3;
 if (x === 3) {
   var x = 2;
-  console.log(x);
+  console.log(x); // 2
 }
-console.log(x);
+console.log(x); // 2
 
+// the first x = 2 : just print var x value
+// the second x = 2: because of hoisting, you declare a variable 
+// anywhere within a function, it is as if it was declared at the top of the function.
