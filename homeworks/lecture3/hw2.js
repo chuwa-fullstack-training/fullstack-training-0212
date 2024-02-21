db9d288703a987e77de0f8541583229403f8ad97
CSHangyuanXu <47225065+CSHangyuanXu@users.noreply.github.com>
2024-02-21 15:40:58 -0800

    Update hw2.js

--- homeworks/lecture3/hw2.js
+++ homeworks/lecture3/hw2.js
@@ -2,6 +2,17 @@
  * console.log(sum(2)(3) === 5)
  * console.log(sum(2, 3) === 5)
  */
-function sum() {
-    // implement here
+ function sum(...args) {
+  if (args.length === 2) {
+    return args[0] + args[1];
+  } else if (args.length === 1) {
+    return function(y) {
+      return args[0] + y;
+    };
+  } else {
+    return undefined; 
+  }
 }
+
+console.log(sum(2)(3) === 5)
+console.log(sum(2, 3) === 5)
