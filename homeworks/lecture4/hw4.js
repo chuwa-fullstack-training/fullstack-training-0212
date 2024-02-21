e78122706a6a15d37169fa4bda3165b6ef79ec0d
xuze19 <159867185+xuze19@users.noreply.github.com>
2024-02-21 14:35:29 -0500

    Update hw4.js

--- homeworks/lecture4/hw4.js
+++ homeworks/lecture4/hw4.js
@@ -1,3 +1,5 @@
+/**
+ * Given two integer arrays nums1 and nums2, return an array of their intersection. Each element in the result must be unique and you may return the result in any order.
  *
  * Example 1:
  * Input: nums1 = [1,2,2,1], nums2 = [2,2]
