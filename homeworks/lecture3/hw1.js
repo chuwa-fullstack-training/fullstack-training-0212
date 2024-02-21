828567a65c2b3adb43611e0533dc28ca6629c739
CSHangyuanXu <47225065+CSHangyuanXu@users.noreply.github.com>
2024-02-21 15:37:17 -0800

    Update hw1.js

--- homeworks/lecture3/hw1.js
+++ homeworks/lecture3/hw1.js
@@ -3,6 +3,24 @@
  * pick 48 coins to have 1 dollar
  * print out 2 solutions
  */
-function pickCoins() {
-    // implement here
+ function pickCoins() {
+  helper(100, 48, []);
 }
+
+function helper(rest, count, list) {
+  if (count === 0 && rest === 0) {
+    console.log(list);
+    return;
+  }
+
+  if (count === 0 || rest < 0) {
+    return;
+  }
+
+  helper(rest - 1, count - 1, list.concat([1]));
+  helper(rest - 5, count - 1, list.concat([5]));
+  helper(rest - 25, count - 1, list.concat([25]));
+  helper(rest - 50, count - 1, list.concat([50]));
+}
+
+pickCoins();
