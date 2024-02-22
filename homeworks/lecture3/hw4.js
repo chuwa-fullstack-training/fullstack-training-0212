3594f30f794814a51ce7a5af56766f06162f4509
CSHangyuanXu <47225065+CSHangyuanXu@users.noreply.github.com>
2024-02-21 16:35:17 -0800

    Update hw4.js

--- homeworks/lecture3/hw4.js
+++ homeworks/lecture3/hw4.js
@@ -1,16 +1,16 @@
 function Shape() {
-    this.type = 'shape';
+  this.type = 'shape';
 }
 
 Shape.prototype.getType = function() {
-    return this.type;
+  return this.type;
 }
 
 function Triangle(a, b, c) {
-    this.type = 'triangle';
-    this.a = a;
-    this.b = b;
-    this.c = c;
+  this.type = 'triangle';
+  this.a = a;
+  this.b = b;
+  this.c = c;
 }
 
 Triangle.prototype = Object.create(Shape.prototype);
@@ -18,10 +18,81 @@ Triangle.prototype.constructor = Triangle;
 
 // your code goes here
 // 1. implement a method getPerimeter for Triangle class
+Triangle.prototype.getPerimeter = function getPerimeterT(a, b){
+  return this.a + this.b + this.c;
+}
+
+
 // 2. implement a method getArea for Triangle class
+Triangle.prototype.getArea = function() {
+  const s = (this.a + this.b + this.c) / 2;
+  return Math.sqrt(s * (s - this.a) * (s - this.b) * (s - this.c));
+}
 
 // 3. implement a new class Circle. this class should inherit from Shape class, and have a radius property.
+function Circle(r) {
+  this.type = 'circle';
+  this.r = r;
+}
+
+Circle.prototype = Object.create(Shape.prototype);
+Circle.prototype.constructor = Circle;
+
 // 4. implement a method area for Circle class
+Circle.prototype.getArea = function() {
+  return Math.PI * this.r * this.r;
+}
+
 // 5. implement a method circumference for Circle class
+Circle.prototype.getCircumference = function() {
+  return 2 * Math.PI * this.r;
+}
 
 // 6. change all code above to use ES6 class syntax
+class Shape {
+  constructor() {
+      this.type = 'shape';
+  }
+
+  getType() {
+      return this.type;
+  }
+}
+
+class Triangle extends Shape {
+  constructor(a, b, c) {
+      super();
+      this.type = 'triangle';
+      this.a = a;
+      this.b = b;
+      this.c = c;
+  }
+
+  getPerimeter() {
+      return this.a + this.b + this.c;
+  }
+
+  getArea() {
+      const s = (this.a + this.b + this.c) / 2;
+      return Math.sqrt(s * (s - this.a) * (s - this.b) * (s - this.c));
+  }
+}
+
+class Circle extends Shape {
+  constructor(radius) {
+      super();
+      this.type = 'circle';
+      this.radius = radius;
+  }
+
+  getArea() {
+      return Math.PI * this.radius * this.radius;
+  }
+
+  getCircumference() {
+      return 2 * Math.PI * this.radius;
+  }
+}
+
+
+
