ffa628a17ce1885befaaf577f4d8c7f259e9b234
xuze19 <159867185+xuze19@users.noreply.github.com>
2024-02-22 03:59:57 -0500

    Update hw5.js

--- homeworks/lecture4/hw5.js
+++ homeworks/lecture4/hw5.js
@@ -6,6 +6,21 @@
 // }
 // data.child = data;
 
+const cloned = new Map(); 
 const cloneDeepWithLoop = (obj) => {
     // Implement the function here
+    if (obj === null || typeof obj !== 'object') {
+        return obj;
+    }
+    if (cloned.has(obj)) {
+        return cloned.get(obj);
+    }
+    const clone = {};
+    cloned.set(obj, clone)
+    for (const key in obj) {
+        if (obj.hasOwnProperty(key)) {
+            clone[key] = cloneDeepWithLoop(obj[key])
+        }
+    }
+    return clone;
 }
