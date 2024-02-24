b96136030ef8a6cc2c6294a4191ce1cdac1077ab
machearn <machearn.n@gmail.com>
2024-02-23 19:05:55 -0500

    feat: finished hw3 of lec5

--- homeworks/lecture5/hw3.js
+++ homeworks/lecture5/hw3.js
@@ -1,24 +1,29 @@
 // what is the output in order? and explain why?
 
 // 1
-console.log('a');
-setTimeout(() => console.log('b'), 0);
-console.log('c');
+console.log("a");
+setTimeout(() => console.log("b"), 0);
+console.log("c");
 new Promise((resolve, reject) => {
-  resolve('d');
-  console.log('e');
-  reject('f');
-}).then(result => console.log(result));
+  resolve("d");
+  console.log("e");
+  reject("f");
+}).then((result) => console.log(result));
+// output: a c e d b
+// explain: a will be printed first, then the setTimeout will be added to the callback queue,
+// then c will be printed, then the promise will be executed, e will be printed, then resolve will be called,
 
 // 2
 const fn = () =>
   new Promise((resolve, reject) => {
     console.log(1);
-    resolve('success');
+    resolve("success");
   });
 
-fn().then(res => {
+fn().then((res) => {
   console.log(res);
 });
 
-console.log('start');
+console.log("start");
+// output: start 1 success
+// explain: start will be printed first, then the fn will be executed, 1 will be printed, then resolve will be called
