6634029b725df8018896ee1643dd30029a732ed5
machearn <machearn.n@gmail.com>
2024-02-23 19:06:17 -0500

    feat: finished hw5 of lec5

--- homeworks/lecture5/hw5.js
+++ homeworks/lecture5/hw5.js
@@ -1,6 +1,6 @@
 // change http request into promise-based function
 
-const https = require('https');
+const https = require("https");
 
 // function httpsRequest(url) {
 //   const options = {
@@ -39,8 +39,37 @@ const https = require('https');
 
 function getJSON(url) {
   // implement your code here
+  return new Promise((resolve, reject) => {
+    const options = {
+      headers: {
+        "User-Agent": "request",
+      },
+    };
+    const request = https.get(url, options, (response) => {
+      if (response.statusCode !== 200) {
+        reject(
+          `Did not get an OK from the server. Code: ${response.statusCode}`,
+        );
+        response.resume();
+      }
+      let data = "";
+      response.on("data", (chunk) => {
+        data += chunk;
+      });
+      response.on("end", () => {
+        try {
+          resolve(JSON.parse(data));
+        } catch (e) {
+          reject(e.message);
+        }
+      });
+    });
+    request.on("error", (err) => {
+      reject(`Encountered an error trying to make a request: ${err.message}`);
+    });
+  });
 }
 
-getJSON('https://api.github.com/search/repositories?q=javascript')
-  .then(response => console.log(response.items.length)) // output: 30
-  .catch(err => console.log(err)); // if you remove options from https.get parameters, you might see an error
+getJSON("https://api.github.com/search/repositories?q=javascript")
+  .then((response) => console.log(response.items.length)) // output: 30
+  .catch((err) => console.log(err)); // if you remove options from https.get parameters, you might see an error
