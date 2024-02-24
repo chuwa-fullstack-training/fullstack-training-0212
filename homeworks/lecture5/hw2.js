bd2344cd8df9835fb970dfd847fc726b111a27b5
machearn <machearn.n@gmail.com>
2024-02-23 19:05:44 -0500

    feat: finished hw2 of lec5

--- homeworks/lecture5/hw2.js
+++ homeworks/lecture5/hw2.js
@@ -9,7 +9,25 @@
  */
 function reverseWords(str) {
   // your code here
+  str.reverse();
+
+  let start = 0;
+  let end = 0;
+
+  while (start < str.length) {
+    while (str[end] !== " " && end < str.length) {
+      end++;
+    }
+    for (let i = start, j = end - 1; i < j; i++, j--) {
+      [str[i], str[j]] = [str[j], str[i]];
+    }
+    start = end + 1;
+    end = start;
+  }
+
+  return str.join("");
 }
 
-const input = 'the sky is blue'.split(''); // ['t', 'h', 'e', ' ', 's', 'k', 'y', ' ', 'i', 's', ' ', 'b', 'l', 'u', 'e']
+const input = "the sky is blue".split(""); // ['t', 'h', 'e', ' ', 's', 'k', 'y', ' ', 'i', 's', ' ', 'b', 'l', 'u', 'e']
 reverseWords(input);
+
