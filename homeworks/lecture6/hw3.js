0f9e76892dc99b9519a95deb4e9993f92a410550
machearn <machearn.n@gmail.com>
2024-02-25 22:57:52 -0500

    feat: finished hw3 of lec6

--- homeworks/lecture6/hw3.js
+++ homeworks/lecture6/hw3.js
@@ -8,13 +8,20 @@
  * const debouncedFn = debounce(printHello, 1000)
  * debouncedFn()
  * debouncedFn() // timer reset to 1s
- * 
+ *
  * @param {function} func
  * @param {number} delay
  * @returns {function}
  */
 function debounce(func, delay) {
   // your code here
+  let id;
+  return function () {
+    if (id) {
+      clearTimeout(id);
+    }
+    id = setTimeout(func, delay);
+  };
 }
 
 /**
@@ -27,11 +34,18 @@ function debounce(func, delay) {
  * const throttledFn = throttle(printHello, 1000)
  * throttledFn()
  * throttledFn() // ignored
- * 
+ *
  * @param {function} func
  * @param {number} delay
  * @returns {function}
  */
 function throttle(func, delay) {
   // your code here
+  let id;
+
+  return function () {
+    if (!id) {
+      id = setInterval(func, delay);
+    }
+  };
 }
