3cb0ed09383618c7f4cb20e8fe7148c91f95d688
xuze19 <159867185+xuze19@users.noreply.github.com>
2024-02-26 05:04:21 -0500

    Update hw2.js

--- homeworks/lecture5/hw2.js
+++ homeworks/lecture5/hw2.js
@@ -9,7 +9,24 @@
  */
 function reverseWords(str) {
   // your code here
+  function reverseStr (str, l, r) {
+    while (l < r){
+      [str[l], str[r]] = [str[r], str[l]];
+      l++;
+      r--;
+    }
+  }
+  reverseStr (str, 0, str.length-1);
+  let i = 0;
+  for (let end = 1; end < str.length; end++) {
+    if (str[end] === ' '){
+      reverseStr(str, i, end-1);
+      i = end+1;
+    }
+  }
+  reverseStr(str, i, str.length-1) 
 }
 
 const input = 'the sky is blue'.split(''); // ['t', 'h', 'e', ' ', 's', 'k', 'y', ' ', 'i', 's', ' ', 'b', 'l', 'u', 'e']
-reverseWords(input);
+reverseWords(input)
+console.log(input.join(''))
