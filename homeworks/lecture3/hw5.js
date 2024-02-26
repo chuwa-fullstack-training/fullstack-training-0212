6b917db12afad7e7c00bc7e09cc5093b09aab298
CSHangyuanXu <47225065+CSHangyuanXu@users.noreply.github.com>
2024-02-26 12:43:51 -0800

    Update hw5.js

--- homeworks/lecture3/hw5.js
+++ homeworks/lecture3/hw5.js
@@ -11,6 +11,23 @@
  * user.checkPassword('123'); // false
  * user.password; // undefined
  */
-function User() {
-    // implement here
+ function User() {
+  // implement here
+  let password;
+  return{
+    setPassword: function(newPassword) {
+    try {
+      if (!password) {
+        password = newPassword;
+      } else {
+        throw new Error("Password is already set");
+      }
+    } catch (error) {
+      console.error(error.message);
+    }
+  },
+    checkPassword: function(inputPassword) {
+      return password === inputPassword;
+    }
+  };
 }
