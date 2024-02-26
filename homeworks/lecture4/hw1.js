e81b2c8b2dcab071ea354af03657f38adae72e74
CSHangyuanXu <47225065+CSHangyuanXu@users.noreply.github.com>
2024-02-26 12:58:36 -0800

    Update hw1.js

--- homeworks/lecture4/hw1.js
+++ homeworks/lecture4/hw1.js
@@ -6,5 +6,21 @@
 // <html><head><title>My Title</title></head></html - true
 
 function checkValidHTML(html) {
-    // implement your solution here
+  // implement your solution here
+  const stack = [];
+  for (let i = 0; i < html.length; i++) {
+    if (html[i] === '<') {
+      if (html[i + 1] === '/') {
+        const closingTag = html.substring(i + 2, html.indexOf('>', i + 2));
+        if (stack.length === 0 || stack.pop() !== closingTag) {
+          return false;
+        }
+      } else {
+        const openingTag = html.substring(i + 1, html.indexOf('>', i + 1));
+        stack.push(openingTag);
+      }
+    }
+  }
+
+  return stack.length === 0;
 }
