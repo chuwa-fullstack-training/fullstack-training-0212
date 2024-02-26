bba4b34171d76bc518c4fe186ffb1e93cb030b6a
xuze19 <159867185+xuze19@users.noreply.github.com>
2024-02-25 23:20:35 -0500

    Update hw4.js

--- homeworks/lecture6/hw4.js
+++ homeworks/lecture6/hw4.js
@@ -6,4 +6,23 @@
  */
 function format(num) {
   // your code here
+  let numStr = num.toString();
+  
+  let divide = numStr.split('.');
+  let integer = divide[0];
+  let fractor = divide[1];
+  
+  let ans = '';
+  for (let i = integer.length - 1, cnt = 0; i >= 0; i--){
+    ans = integer[i] + ans;
+    cnt ++;
+    if (cnt == 3 && i !== 0) {
+      cnt = 0;
+      ans = ',' + ans;
+    }
+  }
+  if (fractor) {
+    return ans + '.' + fractor;
+  }
+  return ans;
 }
