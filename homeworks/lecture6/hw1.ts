17a32bc45aef235ef6fafa8169ea6aa6c96f9ac4
machearn <machearn.n@gmail.com>
2024-02-25 22:57:05 -0500

    feat: finished hw1 of lec6

--- homeworks/lecture6/hw1.ts
+++ homeworks/lecture6/hw1.ts
@@ -10,6 +10,14 @@ function makeCustomer<T extends User>(u: T): T {
     type: "customer",
   };
 }
+// error: type User can't be assigned to type T. T is a generic type that extends User. It may contain more properties than User
+// fix
+function fixedMakeCustomer(u: User): User {
+  return {
+    id: u.id,
+    type: "customer",
+  };
+}
 
 // 2. fix the following code
 // requirement: the function should accept either two strings or two numbers at the same time,
@@ -19,5 +27,15 @@ function f(a: string | number, b: string | number) {
     return `${a} : ${b}`;
   } else {
     return a + b;
+  }
+}
+
+function fixedF(a: string | number, b: string | number) {
+  if (typeof a === "string" && typeof b === "string") {
+    return `${a} : ${b}`;
+  } else if (typeof a === "number" && typeof b === "number") {
+    return a + b;
+  } else {
+    throw new Error("invalid input");
   }
 }
