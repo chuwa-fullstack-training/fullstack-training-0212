9a73d3abfc7076c7f62b2c515f826eb6126e832c
xuze19 <159867185+xuze19@users.noreply.github.com>
2024-02-26 03:16:15 -0500

    Update hw5.js

--- homeworks/lecture6/hw5.js
+++ homeworks/lecture6/hw5.js
@@ -1,7 +1,14 @@
 // 1. use `promise` to print 1, 2, 3 in every 1 second
-function print() {
+function print(i) {
   // your code here
+  return new Promise(function (resolve) {
+    setTimeout(() => {   
+      console.log(i);
+      resolve();
+    }, 1000)
+  })
 }
+print(1).then(() => print(2)).then(() => print(3));
 
 // improved: print every single numbers in a list in every 1 second
 // hint: `reduce`
@@ -9,11 +16,34 @@ const nums = [3, 1, 6, 9, 2];
 
 function printList() {
   // your code here
+  (nums).reduce((promise, cur) => {
+    return promise.then(() => new Promise(resolve => {
+      setTimeout (() => {
+        console.log(cur);
+        resolve();    //即在一秒后，才标志着这个 Promise 已经被解决（resolved）了
+      }, 1000);  
+    }));
+  }, Promise.resolve())
 }
+printList(nums);
 
 // 2. traffic light
 // output: red -> green -> yellow -> red -> ...
 // the delay time is up to you, but the order has to be correct
-function trafficLight() {
+async function trafficLight() {
   // your code here
+  const lights = ['red', 'green', 'yellow'];
+  const last = [5000, 4000, 2000];
+  
+  while (true) {
+    for (let i of lights) {
+      await new Promise(resolve => {
+        setTimeout (() => {
+          console.log(i);
+          resolve();
+        }, last[lights.indexOf(i)]);
+      })
+    }
+  }
 }
+trafficLight()
