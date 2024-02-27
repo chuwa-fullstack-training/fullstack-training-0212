dbb1aed2e1a56f89f7d764c90aea18f70176e778
machearn <machearn.n@gmail.com>
2024-02-26 23:22:27 -0500

    feat: finished hw2 of lec7

--- homeworks/lecture7/hw2.js
+++ homeworks/lecture7/hw2.js
@@ -19,3 +19,26 @@
  */
 
 // your code here
+const http = require("http");
+const url = require("url");
+
+const server = http.createServer((req, res) => {
+  const parsedUrl = url.parse(req.url, true);
+  const iso = parsedUrl.query.iso;
+  const date = new Date(iso);
+  let result;
+  if (parsedUrl.pathname === "/api/parsetime") {
+    result = {
+      hour: date.getHours(),
+      minute: date.getMinutes(),
+      second: date.getSeconds(),
+    };
+  } else if (parsedUrl.pathname === "/api/unixtime") {
+    result = {
+      unixtime: date.getTime(),
+    };
+  }
+  res.writeHead(200, { "Content-Type": "application/json" });
+  res.end(JSON.stringify(result));
+});
+
