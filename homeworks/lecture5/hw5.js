791e21c4ad05821f431ccf1f70c3aaf1a304f34e
xuze19 <159867185+xuze19@users.noreply.github.com>
2024-02-27 01:39:52 -0500

    Update hw5.js

--- homeworks/lecture5/hw5.js
+++ homeworks/lecture5/hw5.js
@@ -39,6 +39,42 @@ const https = require('https');
 
 function getJSON(url) {
   // implement your code here
+  return new Promise((resolve, reject) => {
+    const options = {
+      headers: {
+        'User-Agent': 'request'
+      }
+    };
+    const request = https.get(url, options, response => {
+      if (response.statusCode !== 200) { 
+        reject(
+          `Did not get an OK from the server. Code: ${response.statusCode}`
+        );
+        response.resume();
+      }
+  
+      let data = '';
+      response.on('data', chunk => {
+        data += chunk;
+      });
+      response.on('end', () => { 
+        try {
+          // When the response body is complete, we can parse it and log it to the console
+          console.log(JSON.parse(data));
+          resolve(JSON.parse(data))
+        } catch (e) { 
+          // If there is an error parsing JSON, log it to the console and throw the error
+          reject = (`JSON parsing fail: ${e.messsage}`);
+          throw new Error(e.message);
+        }
+      });
+    });
+    request.on('error', err => {  
+      reject(
+        `Encountered an error trying to make a request: ${err.message}`
+      );
+    });
+  })
 }
 
 getJSON('https://api.github.com/search/repositories?q=javascript')
