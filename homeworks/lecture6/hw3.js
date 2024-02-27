a5f3d4cb094abdcc350aa9473343e63f0d9ef663
xuze19 <159867185+xuze19@users.noreply.github.com>
2024-02-27 04:56:51 -0500

    Update hw3.js

--- homeworks/lecture6/hw3.js
+++ homeworks/lecture6/hw3.js
@@ -13,9 +13,24 @@
  * @param {number} delay
  * @returns {function}
  */
-function debounce(func, delay) {
+function debounce(func, delay) {  // 点击一次刷新一次，重新发请求
   // your code here
+  let timer;  
+  return function (...args) {
+  //When the returned function is called, it sets a timer using setTimeout.
+    clearTimeout(timer);  // to clear the existing timer, if any
+    //If the returned function is called again before the timer expires, 
+    //the previous timer is cleared, and a new timer is set.
+    timer = setTimeout(() => {
+      func.apply(this, args);
+    }, delay)
+  }
 }
+const printHello = () => console.log('hello')
+const debouncedFn = debounce(printHello, 1000)
+debouncedFn()
+debouncedFn() // timer reset to 1s
+
 
 /**
  * implement throttle function
@@ -32,6 +47,28 @@ function debounce(func, delay) {
  * @param {number} delay
  * @returns {function}
  */
-function throttle(func, delay) {
+function throttle(func, delay) {  //点了只发送一次，不刷新
   // your code here
+  let isThrottle = false;
+  //When the returned function is called, it checks if it's currently throttled (isThrottled)
+  return function(...args) {
+    if (!isThrottle) {
+      func.apply(this, args);
+      isThrottle = true;
+      
+      // to reset isThrottled back to false after the specified delay
+      setTimeout(() => {
+      isThrottle = false;
+      }, delay); 
+  //ensures that the func is called at most once during every delay ms. 
+  //Additional calls within the delay are ignored.
+    } else {
+      console.log('ignored')
+    }
+  }
 }
+const printHello = () => console.log('hello')
+const throttledFn = throttle(printHello, 1000)
+throttledFn()
+throttledFn() // ignored
+setTimeout(() => throttledFn(), 1001)
