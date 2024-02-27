5a3b2d7da5d588b0ac9c870ee0bbcad4f9002e0d
Kangcheng Li <alex.kangchengli@gmail.com>
2024-02-26 17:10:55 -0800

    completed hw1 hw2 hw4

--- homeworks/lecture6/hw1.ts
+++ homeworks/lecture6/hw1.ts
@@ -8,16 +8,18 @@ function makeCustomer<T extends User>(u: T): T {
   return {
     id: u.id,
     type: "customer",
-  };
+  } as T;
 }
 
 // 2. fix the following code
 // requirement: the function should accept either two strings or two numbers at the same time,
 // so if parameters are one string and one number, it should throw an error
 function f(a: string | number, b: string | number) {
-  if (typeof a === "string") {
+  if (typeof a === "string" && typeof b === "string") {
     return `${a} : ${b}`;
-  } else {
+  } else if (typeof a === "number" && typeof b === "number") {
     return a + b;
+  } else {
+    throw new Error("Parameters contain one string and one number")
   }
 }
--- homeworks/lecture6/hw4.js
+++ homeworks/lecture6/hw4.js
@@ -6,4 +6,29 @@
  */
 function format(num) {
   // your code here
+  let num_str = num.toString()
+  let res = [];
+  let decimal_idx = 0;
+  while (decimal_idx < num_str.length) {
+    if (num_str[decimal_idx] === '.') {
+      break;
+    }
+    decimal_idx++;
+  }
+
+  for (let i = num_str.length - 1; i >= decimal_idx; i--) {
+    res.push(num_str[i]);
+  }
+
+  let right = decimal_idx - 1;
+  while (right >= 0) {
+    res.push(num_str.charAt(right));
+    if (right > 0 && (decimal_idx - right) % 3 === 0) {
+      res.push(",");
+    }
+    right--;
+  }
+  return res.reverse().join("")
 }
+
+console.log(format(12345678))
