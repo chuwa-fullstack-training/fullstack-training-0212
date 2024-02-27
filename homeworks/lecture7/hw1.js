6dfd8967759c80531c8c82103b6981f55dd110e2
machearn <machearn.n@gmail.com>
2024-02-26 23:22:17 -0500

    feat: finished hw1 of lec7

--- homeworks/lecture7/hw1.js
+++ homeworks/lecture7/hw1.js
@@ -14,3 +14,19 @@
  */
 
 // your code here
+
+const fs = require("fs");
+
+const directory = process.argv[2];
+const extension = process.argv[3];
+
+fs.readdir(directory, (err, files) => {
+  if (err) {
+    throw err;
+  }
+  files.forEach((file) => {
+    if (file.endsWith(`.${extension}`)) {
+      console.log(file);
+    }
+  });
+});
