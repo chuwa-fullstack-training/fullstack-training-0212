da12f5c6bb636b867417eacd27ab5eef5747aed9
CSHangyuanXu <47225065+CSHangyuanXu@users.noreply.github.com>
2024-02-28 13:53:45 -0800

    Update hw2.js

--- homeworks/lecture5/hw2.js
+++ homeworks/lecture5/hw2.js
@@ -7,8 +7,27 @@
  * extra: in-place
  * @param {string[]} str
  */
-function reverseWords(str) {
+ function reverseWords(str) {
   // your code here
+  for (let i = 0; i < str.length / 2; i++) {
+    let temp = str[i];
+    str[i] = str[str.length - 1 - i];
+    str[str.length - 1 - i] = temp;
+  }
+  
+  let start = 0;
+  for (let i = 0; i <= str.length; i++) {
+    if (i === str.length || str[i] === ' ') {
+      for (let j = start; j < (start + i - 1) / 2; j++) {
+        let temp = str[j];
+        str[j] = str[i + start - 1 - j];
+        str[i + start - 1 - j] = temp;
+      }
+      start = i + 1;
+    }
+  }
+  console.log(str.join(''));
+  
 }
 
 const input = 'the sky is blue'.split(''); // ['t', 'h', 'e', ' ', 's', 'k', 'y', ' ', 'i', 's', ' ', 'b', 'l', 'u', 'e']
