94cf81c7654a87f1c772926a7bcc000255589d45
CSHangyuanXu <47225065+CSHangyuanXu@users.noreply.github.com>
2024-02-29 15:24:48 -0800

    Update hw4.js

--- homeworks/lecture6/hw4.js
+++ homeworks/lecture6/hw4.js
@@ -4,13 +4,10 @@
  * example: 1234.56 => 1,234.56
  * @param {number} num
  */
- function format(num):string{
-  // your code here
-  let temp : string = Math.floor(num).toLocaleString();
-
-  let char : string = num.toString();
-  let array : string[] = char.split('.');
-  return num % 1 === 0 ? temp : temp + '.' + array[1];
+function format(num) {
+  let temp = Math.floor(num).toLocaleString();
+  let parts = num.toString().split('.');
+  return parts.length === 1 ? temp : temp + '.' + parts[1];
 }
 console.log(format(12345678)); // Outputs: "12,345,678"
 console.log(format(1234.56));   // Outputs: "1,234.56"
