a69ac3d052f0350e262fa30ee1fc6596fce83048
CSHangyuanXu <47225065+CSHangyuanXu@users.noreply.github.com>
2024-02-29 15:28:43 -0800

    Update hw5.js

--- homeworks/lecture6/hw5.js
+++ homeworks/lecture6/hw5.js
@@ -1,48 +1,40 @@
-function print(num: number): Promise<void> {
-  return new Promise<void>((resolve) => {
-    setTimeout(() => {
-      resolve();
-    }, 1000);
-  });
-}
-
-
 // 1. use `promise` to print 1, 2, 3 in every 1 second
-
-async function printNumbers() {
-  for (let i = 1; i <= 3; i++) {
-    await print(i);
-    console.log(i);
+function print() {
+    // your code here
+    let num = 0;
+    setInterval(() => {
+      console.log(++num);
+    }, 1000);
   }
-}
-
-// printNumbers();
-
-// improved: print every single numbers in a list in every 1 second
-// hint: `reduce`
-// const nums = [3, 1, 6, 9, 2];
-async function printList(nums: number[]): Promise<void> {
-  await nums.reduce(async (previousPromise, num) => {
-    await previousPromise;
-    console.log(num);
-    await print(1000);
-  }, Promise.resolve());
-}
-const nums = [3, 1, 6, 9, 2];
-printList(nums);
-
-// 2. traffic light
-// output: red -> green -> yellow -> red -> ...
-// the delay time is up to you, but the order has to be correct
-async function trafficLight() : Promise<void> {
-  // your code here
-  const color = ['red', 'green','yellow'];
-  let x: number= 0;
-  while(true) {
-    await print(1000);
-    console.log(color[x % 3] );
-    x++;
+  
+  // improved: print every single numbers in a list in every 1 second
+  // hint: `reduce`
+  const nums = [3, 1, 6, 9, 2];
+  
+  function printList() {
+    // your code here
+    nums.reduce((promise, num) => {
+        return promise.then(() => {
+          return new Promise((resolve) => {
+            setTimeout(() => {
+              console.log(num);
+              resolve();
+            }, 1000);
+          });
+        });
+      }, Promise.resolve());
   }
-}
-trafficLight();
+  
+  // 2. traffic light
+  // output: red -> green -> yellow -> red -> ...
+  // the delay time is up to you, but the order has to be correct
+  function trafficLight() {
+    // your code here
+    const colors = ['red', 'green', 'yellow'];
+    let index = 0;
 
+    setInterval(() => {
+        console.log(colors[index]);
+        index = (index + 1) % 3;
+    }, 1000);
+  }
