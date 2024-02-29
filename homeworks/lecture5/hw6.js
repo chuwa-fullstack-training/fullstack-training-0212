b3e7d79de7cc302f2d95614a1aeed245c9c01472
CSHangyuanXu <47225065+CSHangyuanXu@users.noreply.github.com>
2024-02-28 22:15:13 -0800

    Update hw6.js

--- homeworks/lecture5/hw6.js
+++ homeworks/lecture5/hw6.js
@@ -4,7 +4,9 @@
  * @param {string[]} urls - an array of urls
  * @returns {any[]} - an array of responses
  */
-function sequencePromise(urls) {
+ const fetch = require('node-fetch');
+ const https = require('https');
+ function sequencePromise(urls) {
   const results = [];
   function fetchOne(url) {
     // for `getJSON` function you can choose either from the implementation of hw5 or `fetch` version provided by browser
@@ -12,19 +14,74 @@ function sequencePromise(urls) {
     return getJSON(url).then(response => results.push(response));
   }
   // implement your code here
+  //option 1
+  // return urls.reduce((previousPromise, url) => {
+  //   return previousPromise.then(() => fetchOne(url));
+  // }, Promise.resolve())
+  // .then(() => results);
 
-  return results;
+
+
+  //option 2
+  const promises = urls.map(url => fetchOne(url));
+
+  return Promise.all(promises).then(() => results);
 }
 
 // option 1
-function getJSON(url) {
-  // this is from hw5
-}
+// function getJSON(url) {
+//   // this is from hw5
+//   const options = {
+//     headers: {
+//       'User-Agent': 'request'
+//     }
+//   };
+
+//   return new Promise((resolve, reject) => {
+//     const request = https.get(url, options, response => {
+//       let data = '';
+
+//       response.on('data', chunk => {
+//         data += chunk;
+//       });
+
+//       response.on('end', () => {
+//         try {
+//           const jsonData = JSON.parse(data);
+//           resolve(jsonData);
+//         } catch (e) {
+//           reject(new Error(e.message));
+//         }
+//       });
+//     });
+
+//     request.on('error', err => {
+//       reject(new Error(`Encountered an error trying to make a request: ${err.message}`));
+//     });
+//   });
+// }
 
 // option 2
-// function getJSON(url) {
-//     return fetch(url).then(res => res.json());
-// }
+
+
+function getJSON(url) {
+  const options = {
+    headers: {
+      'User-Agent': 'request'
+    }
+  };
+
+  return fetch(url, options)
+    .then(response => {
+      if (!response.ok) {
+        throw new Error(`Did not get an OK from the server. Code: ${response.status}`);
+      }
+      return response.json();
+    })
+    .catch(error => {
+      throw new Error(`Error during fetch: ${error.message}`);
+    });
+}
 
 // test your code
 const urls = [
@@ -32,3 +89,11 @@ const urls = [
   'https://api.github.com/search/repositories?q=react',
   'https://api.github.com/search/repositories?q=nodejs'
 ];
+
+sequencePromise(urls)
+  .then(results => {
+    console.log(results);
+  })
+  .catch(error => {
+    console.error(error);
+  });
