408b7c2dd09b71d239f47a9e5e44b5dfd5427641
machearn <machearn.n@gmail.com>
2024-03-01 17:05:21 -0500

    feat: finished hw3 of lec8

--- homeworks/lecture8/hw3.js
+++ homeworks/lecture8/hw3.js
@@ -1,3 +1,35 @@
 /**
  * Refactor hw3 in lecture 7 to use Express.js with template engine.
  */
+
+const express = require("express");
+
+const app = express();
+
+app.set("view engine", "pug");
+app.set("views", "./views");
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+  res.end("this is the home page");
+});
+
+app.get("/about", (req, res) => {
+  res.end("this is the about page");
+});
+
+app.get("/home", (req, res) => {
+  res.render("home");
+});
+
+app.post("/create-post", (req, res) => {
+  const title = req.body.title;
+  const content = req.body.content;
+
+  res.render("home", { title: title, content: content });
+});
+
+app.listen(3000, () => console.log("Example app listening on port 3000!"));
+
