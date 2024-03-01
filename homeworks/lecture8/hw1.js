a244228e761900596a09963dda7b543779666bc9
machearn <machearn.n@gmail.com>
2024-03-01 17:04:52 -0500

    feat: finished hw1 of lec8

--- homeworks/lecture8/hw1.js
+++ homeworks/lecture8/hw1.js
@@ -9,3 +9,51 @@
  *    You don't need to handle the case like http://localhost:3000/hw1/test/test/txt.
  * 3. hw2 should be able to handle requests with query strings like it did in lecture 7;
  */
+const express = require("express");
+const path = require("path");
+const fs = require("fs");
+
+const hw1Router = express.Router();
+const hw2Router = express.Router();
+
+const app = express();
+
+app.use("/hw1", hw1Router);
+app.use("/hw2", hw2Router);
+
+let baseDir = path.dirname(path.dirname(__dirname));
+
+hw1Router.get("/:dir/:ext", (req, res) => {
+  const directory = path.join(baseDir, req.params.dir);
+  const extension = req.params.ext;
+
+  fs.readdir(directory, (err, files) => {
+    if (err) {
+      throw err;
+    }
+    let result = files.filter((file) => file.endsWith(`.${extension}`));
+    res.send(result);
+  });
+});
+
+hw2Router.get("/parsetime", (req, res) => {
+  const iso = req.query.iso;
+  const date = new Date(iso);
+  res.json({
+    hour: date.getHours(),
+    minute: date.getMinutes(),
+    second: date.getSeconds(),
+  });
+});
+
+hw2Router.get("/unixtime", (req, res) => {
+  const iso = req.query.iso;
+  const date = new Date(iso);
+  res.json({
+    unixtime: date.getTime(),
+  });
+});
+
+app.listen(3000, () => {
+  console.log("Server is running on http://localhost:3000");
+});
