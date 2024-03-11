12cff6d3949701102bd705b1958c22ee28c37584
CSHangyuanXu <47225065+CSHangyuanXu@users.noreply.github.com>
2024-03-10 20:17:50 -0700

    Update hw3.js

--- homeworks/lecture8/hw3.js
+++ homeworks/lecture8/hw3.js
@@ -1,3 +1,32 @@
 /**
  * Refactor hw3 in lecture 7 to use Express.js with template engine.
  */
+
+
+const express = require('express');
+const path = require('path');
+const app = express();
+const port = 3000;
+const fs = require('fs');
+
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.urlencoded({ extended: true }));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'home.html'));
+});
+
+app.post('/', (req, res) => {
+    const title = req.body.title;
+    const content = req.body.content;
+    let html = '';
+    const htmlpath = path.join(__dirname, 'home.html');
+    html =  fs.readFileSync(htmlpath, 'utf8');
+    const updatedHtml = html.toString().replace('</body>', `<p>Title: ${title}</p><p> Content: ${content}</p></body>`);
+
+    res.send(updatedHtml);
+});
+
+app.listen(port, () => {
+    console.log(`Server is running at http://localhost:${port}`);
+});
