e2f8eda7c1304eb24aa586eb8235d425c09a8acb
machearn <machearn.n@gmail.com>
2024-03-12 14:53:53 -0400

    feat: finished hw2 of lec13

---
+++ homeworks/lecture13/hw2/src/App.jsx
@@ -0,0 +1,83 @@
+function App() {
+  return (
+    <div className="flex flex-col h-5/6 w-1/3 m-auto bg-white items-center">
+      <header className="w-full bg-gray-600 text-center text-white">
+        header bar
+      </header>
+      <div className="flex-1 w-full bg-gray-200 flex flex-col justify-around">
+        <div className="flex justify-around m-5">
+          <button className="bg-cyan-400 p-5 rounded-md font-bold text-gray-100 text-xl w-16 h-16">
+            1
+          </button>
+          <button className="bg-cyan-400 p-5 rounded-md font-bold text-gray-100 text-xl w-16 h-16">
+            2
+          </button>
+          <button className="bg-cyan-400 p-5 rounded-md font-bold text-gray-100 text-xl w-16 h-16">
+            3
+          </button>
+          <button className="bg-cyan-400 p-5 rounded-md font-bold text-gray-100 text-xl w-16 h-16">
+            4
+          </button>
+        </div>
+        <div className="flex justify-around m-5">
+          <button className="bg-cyan-400 p-5 rounded-md font-bold text-gray-100 text-xl w-16 h-16">
+            5
+          </button>
+          <button className="bg-cyan-400 p-5 rounded-md font-bold text-gray-100 text-xl w-16 h-16">
+            6
+          </button>
+          <button className="bg-cyan-400 p-5 rounded-md font-bold text-gray-100 text-xl w-16 h-16">
+            7
+          </button>
+          <button className="bg-cyan-400 p-5 rounded-md font-bold text-gray-100 text-xl w-16 h-16">
+            8
+          </button>
+        </div>
+        <div className="flex justify-around m-5">
+          <button className="bg-cyan-400 p-5 rounded-md font-bold text-gray-100 text-xl w-16 h-16">
+            9
+          </button>
+          <button className="bg-cyan-400 p-5 rounded-md font-bold text-gray-100 text-xl w-16 h-16">
+            10
+          </button>
+          <button className="bg-cyan-400 p-5 rounded-md font-bold text-gray-100 text-xl w-16 h-16">
+            11
+          </button>
+          <button className="bg-cyan-400 p-5 rounded-md font-bold text-gray-100 text-xl w-16 h-16">
+            12
+          </button>
+        </div>
+        <div className="flex justify-around m-5">
+          <button className="bg-cyan-400 p-5 rounded-md font-bold text-gray-100 text-xl w-16 h-16">
+            13
+          </button>
+          <button className="bg-cyan-400 p-5 rounded-md font-bold text-gray-100 text-xl w-16 h-16">
+            14
+          </button>
+          <button className="bg-cyan-400 p-5 rounded-md font-bold text-gray-100 text-xl w-16 h-16">
+            15
+          </button>
+          <button className="bg-cyan-400 p-5 rounded-md font-bold text-gray-100 text-xl w-16 h-16">
+            16
+          </button>
+        </div>
+      </div>
+      <div className="flex w-full justify-around m-8">
+        <button className="bg-cyan-400 p-5 rounded-md font-bold text-gray-100 text-xl w-16 h-16">
+          17
+        </button>
+        <button className="bg-cyan-400 p-5 rounded-md font-bold text-gray-100 text-xl w-16 h-16">
+          18
+        </button>
+        <button className="bg-cyan-400 p-5 rounded-md font-bold text-gray-100 text-xl w-16 h-16">
+          19
+        </button>
+        <button className="bg-cyan-400 p-5 rounded-md font-bold text-gray-100 text-xl w-16 h-16">
+          20
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ homeworks/lecture13/hw2/src/main.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <div className="flex flex-col h-screen w-screen bg-gray-100">
+      <App />
+    </div>
+  </React.StrictMode>,
+);
