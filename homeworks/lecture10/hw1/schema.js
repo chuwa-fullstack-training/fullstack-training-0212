95e1abc8e50742cca7805746a02302b24035c12d
xuze19 <159867185+xuze19@users.noreply.github.com>
2024-03-13 14:18:56 -0400

    Add files via upload

---
+++ homeworks/lecture10/hw1/schema.js
@@ -0,0 +1,54 @@
+const mongoose = require('mongoose');
+const {Schema} = mongoose;
+
+const CompanySchema = new Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  description: {
+    type: String,
+    default: 'default content'
+  },
+  headquarters: String,
+  industry: String,
+  employees: [{  
+    type: Schema.Types.ObjectId,  
+    ref: 'Employee'  
+  }]
+});
+
+const EmployeeSchema = new Schema({   //schema = model
+  firstName: {
+    type: String,
+    required: true
+  },
+  lastName: {
+    type: String
+  },
+  company: {
+    type: Schema.Types.ObjectId,
+    ref: 'company'
+  },
+  startDate: {
+    type: Date,
+    default: Date.now
+  },
+  jobTitle: String,
+  resigned: Boolean,
+  salary: Number,
+  manager: {
+    type: Schema.Types.ObjectId,
+    ref: 'Employee'
+  }
+});
+
+const Company = mongoose.model('Company', CompanySchema);   //生成schema
+const Employee = mongoose.model('Employee', EmployeeSchema);
+const LB = mongoose.model('LB', CompanySchema, 'lb'); 
+
+module.exports = {
+    Company,
+    Employee,
+    LB
+};
