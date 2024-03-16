5e855f44dffba20bed0df264a018c77133f467cb
dearalina <dearalina2020@gmail.com>
2024-03-15 17:32:46 -0700

    update

--- homeworks/lecture7/hw3.js
+++ homeworks/lecture7/hw3.js
@@ -13,36 +13,49 @@
 const http = require('http');
 const fs = require('fs');
 const path = require('path');
+const urlModule = require('url');
 
 const server = http.createServer((req, res) => {
     const { url, method } = req;
+    const parsedUrl = urlModule.parse(url, true);
+    const pathName = parsedUrl.pathname;
+    const query = parsedUrl.query;
+
+
     if (method === 'GET') {
-        if (url === '/') {
+        if (pathName === '/') {
             res.end('this is the home page');
-        } else if (url === '/about') {
+        } else if (pathName === '/about') {
             res.end('this is the about page');
-        } else if (url.startsWith('/home.html')) {
+        } else if (pathName.startsWith('/home.html')) {
             fs.readFile(path.join(__dirname, 'home.html'), (err, html) => {
                 if (err) {
                     res.end('error');
                 } else {
                     res.writeHead(200, { 'Content-Type': 'text/html' });
-                    res.write(html);
-                    res.end();
+                    let htmlString = html.toString();
+                    if (Object.keys(query).length > 0) {
+                        const queryParamsHtml = Object.entries(query).map(([key, value]) => `<p>${key}: ${value}</p>`).join('');
+                        htmlString += queryParamsHtml;
+                    }
+                    res.end(htmlString);
                 }
             });
         } else {
             res.end('this is the 404 page');
         }
     } else if (method === 'POST') {
-        if (url === '/create-post') {
+        if (pathName === '/create-post') {
             let body = [];
             req.on('data', chunk => {
                 body.push(chunk);
             });
             req.on('end', () => {
                 const parsedBody = Buffer.concat(body).toString();
-                res.end(parsedBody);
+                const redirectURL = '/home.html?' + Buffer.concat(body).toString();
+                res.statusCode = 302;
+                res.setHeader('Location', redirectURL);
+                res.end();
             });
         } else {
             res.end('this is the 404 page');
