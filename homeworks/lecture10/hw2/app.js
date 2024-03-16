b82d153baa63f56dbed7989d63c41cae63a3cbde
Yida Chen <yic192@pitt.edu>
2024-03-15 21:22:29 -0400

    lecture 9&10&11

--- homeworks/lecture10/hw2/app.js
+++ homeworks/lecture10/hw2/app.js
@@ -1,19 +1,16 @@
-// app.js
 const express = require('express');
 const mongoose = require('mongoose');
 const Todo = require('./models/toDo');
 
 const app = express();
 app.set('view engine', 'ejs');
-app.use(express.json()); // for parsing application/json
+app.use(express.json()); 
 
-// MongoDB connection
 mongoose.connect('mongodb://localhost:27017/todoListDB', {
   useNewUrlParser: true,
   useUnifiedTopology: true
 });
 
-// 添加待办事项
 app.post('/todos', async (req, res) => {
   try {
     const newTodo = new Todo({
@@ -26,18 +23,16 @@ app.post('/todos', async (req, res) => {
   }
 });
 
-// 获取所有待办事项
 app.get('/todos', async (req, res) => {
     try {
       const todos = await Todo.find();
-      res.render('todo', { todos }); // 渲染 todo.ejs 模板
+      res.render('todo', { todos }); 
     } catch (err) {
       res.status(500).json({ message: err.message });
     }
   });
   
 
-// 删除待办事项
 app.delete('/todos/:id', async (req, res) => {
   try {
     const todo = await Todo.findByIdAndDelete(req.params.id);
