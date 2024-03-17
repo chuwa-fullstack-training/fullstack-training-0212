810de4ed69fb277818b95e63e8ee17200a3378bb
“Zi_Yan” <yanzi147@outlook.com>
2024-03-16 20:58:15 -0700

    l7

--- homeworks/lecture7/hw2.js
+++ homeworks/lecture7/hw2.js
@@ -19,3 +19,39 @@
  */
 
 // your code here
+const http = require('http');
+const url = require('url');
+
+const server = http.createServer((request, response) => {
+  const parsedUrl = url.parse(request.url, true);
+  const isoTime = parsedUrl.query.iso;
+  const pathName = parsedUrl.pathname;
+  let result = {};
+
+  const date = new Date(isoTime);
+
+  if (pathName === '/api/parsetime') {
+    result = {
+      hour: date.getHours(),
+      minute: date.getMinutes(),
+      second: date.getSeconds()
+    };
+  } else if (pathName === '/api/unixtime') {
+    result = {
+      unixtime: date.getTime()
+    };
+  } else {
+    // Handle unknown paths
+    response.writeHead(404, { 'Content-Type': 'application/json' });
+    response.end(JSON.stringify({ error: 'Not found' }));
+    return;
+  }
+
+  // Respond with the result
+  response.writeHead(200, { 'Content-Type': 'application/json' });
+  response.end(JSON.stringify(result));
+});
+
+server.listen(3000, () => {
+  console.log('Server listening on port 3000');
+});
--- homeworks/lecture7/hw3.js
+++ homeworks/lecture7/hw3.js
@@ -9,3 +9,52 @@
  * 3. you need to figure out how to parse the query string in the home.html page
  * 4. after writing the html content, you need to write the query string in the html as well
  */
+
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
+const querystring = require('querystring');
+
+const server = http.createServer((req, res) => {
+  const { url, method } = req;
+  if (method === 'GET') {
+    if (url === '/') {
+      res.end('this is the home page');
+    } else if (url === '/about') {
+      res.end('this is the about page');
+    } else if (url.startsWith('/home.html')) {
+      fs.readFile(path.join(__dirname, 'home.html'), (err, html) => {
+        if (err) {
+          res.end('error');
+        } else {
+          res.writeHead(200, { 'Content-Type': 'text/html' });
+          res.write(html);
+          res.end();
+        }
+      });
+    } else {
+      res.end('this is the 404 page');
+    }
+  } else if (method === 'POST') {
+    if (url === '/create-post') {
+      let body = '';
+      req.on('data', chunk => {
+        body += chunk.toString(); // Convert Buffer to string
+      });
+      req.on('end', () => {
+        const postData = querystring.stringify(querystring.parse(body));
+        // Redirect back to home.html with query string
+        res.writeHead(302, { 'Location': `/home.html?${postData}` });
+        res.end();
+      });
+    } else {
+      res.end('this is the 404 page');
+    }
+  } else {
+    res.end('Unsupported method');
+  }
+});
+
+server.listen(3000, () => {
+  console.log('Server is running on port 3000');
+});
